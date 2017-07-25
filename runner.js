84aff8e16d1273c1d120fd125b804cf20620d654
Muhammad Dadu <muhammad@dadu.co>
2017-07-26 00:17:25 +0100

    some changes

--- runner.js
+++ runner.js
@@ -1,39 +1,19 @@
 /**
 * Application Bootstrap for The Box
 */
-const path = require('path');
-const electron = require('electron');
-const {app, powerSaveBlocker, BrowserWindow} = electron;
-
-const INDEX_HTML_PATH = path.join(__dirname, 'app', 'index.html');
-const NODE_ENV = process.env.NODE_ENV || 'development';
-
-// prevent sleep
-let powerSaveBlockerId = powerSaveBlocker.start('prevent-display-sleep');
-let applicationWindow = null;
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => app.quit());
-
-app.on('ready', () => {
-	applicationWindow = new BrowserWindow({
-		width: 800,
-		height: 600,
-		kiosk: NODE_ENV !== 'development',
-		autoHideMenuBar: true,
-		darkTheme: true,
-		backgroundColor: '#021B32'
-	});
-
-	// and load the index.html of the app.
-	applicationWindow.loadURL(`file://${INDEX_HTML_PATH}`);
-
-	// Open the DevTools if run with "npm start dev"
-	if(process.argv[2] === 'dev'){  
-		applicationWindow.webContents.openDevTools();
+const Application = require('./lib/Application');
+const CONFIGURATION = {
+	idDev: process.argv[2] === 'dev' || process.env.NODE_ENV === 'development',
+	rootApplicationHtml: path.join(__dirname, 'app', 'index.html'),
+	entrypointCss: ['style/module.scss'].map((path) => path.resolve(__dirname, path)),
+	entrypointJs: ['app/app.js'].map((path) => path.resolve(__dirname, path)),
+	screenSize: {
+		width: 480,
+		height: 320
 	}
+};
 
-	applicationWindow.on('closed', () => {
-		applicationWindow = null;
-	});
-});
+let application = new Application(CONFIGURATION);
+
+application.initialize();
+application.openApplication();
